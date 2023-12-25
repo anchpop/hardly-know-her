0e4c41d77af319929e652f0e91d281ac8b5cef46
Andre Popovitch <andre@popovit.ch>
2023-12-24 18:38:50 -0600

    two boards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,10 +11,11 @@ export default function Home() {
   const ranks: CardRank[] = ['A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2'];
 
   // State to track the toggled items
-  const [toggledItems, setToggledItems] = useState<Set<String>>(new Set());
+  const [toggledItems1, setToggledItems1] = useState<Set<String>>(new Set());
+  const [toggledItems2, setToggledItems2] = useState<Set<String>>(new Set());
 
   // Function to generate grid items
-  const generateGridItems = () => {
+  const generateGridItems = (toggledItems: Set<String>, setToggledItems: React.Dispatch<React.SetStateAction<Set<String>>>) => {
     let gridItems = [];
     for (let i = 0; i < ranks.length; i++) {
       for (let j = 0; j < ranks.length; j++) {
@@ -26,7 +27,7 @@ export default function Home() {
           <div
             key={itemKey}
             className={`${styles.gridItem} ${toggledItems.has(itemKey) ? styles.toggled : ''}`}
-            onClick={() => toggleItem(itemKey)}
+            onClick={() => toggleItem(itemKey, toggledItems, setToggledItems)}
           >
             {itemDisplay}
           </div>
@@ -37,7 +38,7 @@ export default function Home() {
   };
 
   // Function to handle item toggle
-  const toggleItem = (itemKey: string) => {
+  const toggleItem = (itemKey: string, toggledItems: Set<String>, setToggledItems: React.Dispatch<React.SetStateAction<Set<String>>>) => {
     const newToggledItems = new Set(toggledItems);
     if (toggledItems.has(itemKey)) {
       // make a new set identical to toggledItems but without the item
@@ -49,10 +50,10 @@ export default function Home() {
     setToggledItems(newToggledItems);
   };
 
-  const calculateCombinations: () => {
+  const calculateCombinations: (toggledItems: Set<String>) => {
     combinations: number;
     combinationsByRank: Record<CardRank, number>;
-  } = () => {
+  } = (toggledItems: Set<String>) => {
     let totals = {
       combinations: 0,
       combinationsByRank: {} as Record<CardRank, number>
@@ -83,29 +84,43 @@ export default function Home() {
     return totals;
   };
 
-  const { combinations, combinationsByRank } = calculateCombinations();
+  const comboInfo1 = calculateCombinations(toggledItems1);
+  const comboInfo2 = calculateCombinations(toggledItems2);
 
   return (
     <main className={styles.main}>
-      <div className={styles.grid}>
-        {generateGridItems()}
+      <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-evenly", width: "100%" }}>
+        <div className={styles.grid}>
+          {generateGridItems(toggledItems1, setToggledItems1)}
+        </div>
+        <div className={styles.grid}>
+          {generateGridItems(toggledItems2, setToggledItems2)}
+        </div>
       </div>
       <div>
-        <div>Combos: {combinations}</div>
+        <div>Combos: {comboInfo1.combinations}</div>
       </div>
       <div style={{
-        display: "grid", gridTemplateColumns: "4rem 2rem 3rem", gridGap: "6px", marginTop: "10px",
+        display: "grid", gridTemplateColumns: "4rem 2rem 2rem 3rem 2rem 2rem 4rem", gridGap: "6px", marginTop: "10px",
       }}>
         {ranks.map(rank => (
           <React.Fragment key={rank}>
-            <div style={{
-              borderRight: "1px solid #DEDEDE",
-            }}><span style={{ opacity: 0.5 }}>W/ 1</span> {rank} </div>
             <div>
-              {combinationsByRank[rank]}
+              ({comboInfo1.combinations !== 0 ? Math.round((comboInfo1.combinationsByRank[rank] / comboInfo1.combinations) * 1000) / 10 : 0}%)
             </div>
             <div>
-              ({combinations !== 0 ? Math.round((combinationsByRank[rank] / combinations) * 1000) / 10 : 0}%)
+              {comboInfo1.combinationsByRank[rank]}
+            </div>
+            <div> {rank} </div>
+            <div>
+              <span style={{ opacity: 0.5 }}>W/ 1</span>
+            </div>
+            <div> {rank} </div>
+            <div>
+              {comboInfo2.combinationsByRank[rank]}
+            </div>
+            <div>
+              ({comboInfo2.combinations !== 0 ? Math.round((comboInfo2.combinationsByRank[rank] / comboInfo2.combinations) * 1000) / 10 : 0}%)
             </div>
           </React.Fragment>
         ))}
