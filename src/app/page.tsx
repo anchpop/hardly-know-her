aa0a7312b0ca946f5f959cfb83ec41bb16168493
Andre Popovitch <andre@popovit.ch>
2023-12-29 21:14:18 -0500

    format

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,56 +1,101 @@
-"use client"
+"use client";
 
-import React, { useState } from 'react';
-import Image from 'next/image';
-import styles from './page.module.css';
+import React, { useState } from "react";
+import Image from "next/image";
+import styles from "./page.module.css";
 
-type CardRank = 'A' | 'K' | 'Q' | 'J' | 'T' | '9' | '8' | '7' | '6' | '5' | '4' | '3' | '2';
+type CardRank =
+  | "A"
+  | "K"
+  | "Q"
+  | "J"
+  | "T"
+  | "9"
+  | "8"
+  | "7"
+  | "6"
+  | "5"
+  | "4"
+  | "3"
+  | "2";
 
 function formatNumber(num: number) {
   // Split the number on the decimal point
-  let parts = num.toString().split('.');
+  let parts = num.toString().split(".");
 
   // Pad the integer part to ensure it has at least two digits
-  parts[0] = parts[0].padStart(2, '0');
+  parts[0] = parts[0].padStart(2, "0");
 
   // If there is a decimal part, format it to have exactly one digit
   if (parts[1]) {
-    parts[1] = parts[1].length > 1 ? parts[1].substring(0, 1) : parts[1].padEnd(1, '0');
+    parts[1] =
+      parts[1].length > 1 ? parts[1].substring(0, 1) : parts[1].padEnd(1, "0");
   } else {
     // If there wasn't a decimal part, add ".0"
-    parts.push('0');
+    parts.push("0");
   }
 
   // Join the parts back together
-  return parts.join('.');
+  return parts.join(".");
 }
-
 
 export default function Home() {
   // Define ranks for poker cards
-  const ranks: CardRank[] = ['A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2'];
+  const ranks: CardRank[] = [
+    "A",
+    "K",
+    "Q",
+    "J",
+    "T",
+    "9",
+    "8",
+    "7",
+    "6",
+    "5",
+    "4",
+    "3",
+    "2",
+  ];
 
   // State to track the toggled items
   const [toggledItems1, setToggledItems1] = useState<Set<String>>(new Set());
   const [toggledItems2, setToggledItems2] = useState<Set<String>>(new Set());
 
   // Function to generate grid items
-  const generateGridItems = (toggledItems: Set<String>, setToggledItems: React.Dispatch<React.SetStateAction<Set<String>>>) => {
+  const generateGridItems = (
+    toggledItems: Set<String>,
+    setToggledItems: React.Dispatch<React.SetStateAction<Set<String>>>,
+  ) => {
     let gridItems = [];
     for (let i = 0; i < ranks.length; i++) {
       for (let j = 0; j < ranks.length; j++) {
         // Generate a unique key for each item
         // but always put ace between king and queen, and king between queen and jack, etc.
         const itemKey = `${ranks[j]}${ranks[i]}`;
-        const itemDisplay = (ranks[j] === "A" || (ranks[j] === "K" && ranks[i] !== "A") || (ranks[j] === "Q" && ranks[i] !== "A" && ranks[i] !== "K") || (ranks[j] === "J" && ranks[i] !== "A" && ranks[i] !== "K" && ranks[i] !== "Q") || (ranks[j] === "T" && ranks[i] !== "A" && ranks[i] !== "K" && ranks[i] !== "Q" && ranks[i] !== "J")) ? `${ranks[j]}${ranks[i]}` : `${ranks[i]}${ranks[j]}`;
+        const itemDisplay =
+          ranks[j] === "A" ||
+            (ranks[j] === "K" && ranks[i] !== "A") ||
+            (ranks[j] === "Q" && ranks[i] !== "A" && ranks[i] !== "K") ||
+            (ranks[j] === "J" &&
+              ranks[i] !== "A" &&
+              ranks[i] !== "K" &&
+              ranks[i] !== "Q") ||
+            (ranks[j] === "T" &&
+              ranks[i] !== "A" &&
+              ranks[i] !== "K" &&
+              ranks[i] !== "Q" &&
+              ranks[i] !== "J")
+            ? `${ranks[j]}${ranks[i]}`
+            : `${ranks[i]}${ranks[j]}`;
         gridItems.push(
           <div
             key={itemKey}
-            className={`${styles.gridItem} ${toggledItems.has(itemKey) ? styles.toggled : ''}`}
+            className={`${styles.gridItem} ${toggledItems.has(itemKey) ? styles.toggled : ""
+              }`}
             onClick={() => toggleItem(itemKey, toggledItems, setToggledItems)}
           >
             {itemDisplay}
-          </div>
+          </div>,
         );
       }
     }
@@ -58,13 +103,16 @@ export default function Home() {
   };
 
   // Function to handle item toggle
-  const toggleItem = (itemKey: string, toggledItems: Set<String>, setToggledItems: React.Dispatch<React.SetStateAction<Set<String>>>) => {
+  const toggleItem = (
+    itemKey: string,
+    toggledItems: Set<String>,
+    setToggledItems: React.Dispatch<React.SetStateAction<Set<String>>>,
+  ) => {
     const newToggledItems = new Set(toggledItems);
     if (toggledItems.has(itemKey)) {
       // make a new set identical to toggledItems but without the item
       newToggledItems.delete(itemKey);
-    }
-    else {
+    } else {
       newToggledItems.add(itemKey);
     }
     setToggledItems(newToggledItems);
@@ -76,12 +124,11 @@ export default function Home() {
   } = (toggledItems: Set<String>) => {
     let totals = {
       combinations: 0,
-      combinationsByRank: {} as Record<CardRank, number>
+      combinationsByRank: {} as Record<CardRank, number>,
     };
     for (let rank of ranks) {
       totals.combinationsByRank[rank] = 0;
     }
-
 
     for (let i = 0; i < ranks.length; i++) {
       for (let j = 0; j < ranks.length; j++) {
@@ -90,9 +137,11 @@ export default function Home() {
           const multiplier = i > j ? 12 : i === j ? 6 : 4;
           totals.combinations += multiplier;
 
-          // Check for each rank
+          // Check for each rank 
           for (let rank of ranks) {
-            const exactlyOneRank = (ranks[j] === rank && ranks[i] !== rank) || (ranks[j] !== rank && ranks[i] === rank);
+            const exactlyOneRank =
+              (ranks[j] === rank && ranks[i] !== rank) ||
+              (ranks[j] !== rank && ranks[i] === rank);
             if (exactlyOneRank) {
               totals.combinationsByRank[rank] += multiplier;
             }
@@ -109,7 +158,12 @@ export default function Home() {
 
   const [gameMode, setGameMode] = useState<boolean>(false);
 
-  const [guesses, setGuesses] = useState<("u1" | "u2" | "both" | undefined)[]>(["u1", "u2", "both", undefined]);
+  const [guesses, setGuesses] = useState<("u1" | "u2" | "both" | undefined)[]>([
+    "u1",
+    "u2",
+    "both",
+    undefined,
+  ]);
 
   return (
     <main className={styles.main}>
@@ -125,41 +179,82 @@ export default function Home() {
             {generateGridItems(toggledItems2, setToggledItems2)}
           </div>
           <div>
-            <div className={styles.combos}>Combos: {comboInfo2.combinations}</div>
+            <div className={styles.combos}>
+              Combos: {comboInfo2.combinations}
+            </div>
           </div>
         </div>
       </div>
       <div className={`${styles.gameButtons}`}>
-        <button className={styles.bigButton} onClick={() => {
-          setToggledItems1(new Set());
-          setToggledItems2(new Set());
-        }
-        }>Clear board</button>
-        <button className={styles.bigButton} onClick={() => setGameMode(!gameMode)}>🎮 <b>{gameMode ? "Show Answers" : "Hide Answers"}</b> 🃏</button>
+        <button
+          className={styles.bigButton}
+          onClick={() => {
+            setToggledItems1(new Set());
+            setToggledItems2(new Set());
+          }}
+        >
+          Clear board
+        </button>
+        <button
+          className={styles.bigButton}
+          onClick={() => setGameMode(!gameMode)}
+        >
+          🎮 <b>{gameMode ? "Show Answers" : "Hide Answers"}</b> 🃏
+        </button>
       </div>
-      <div className={`${gameMode ? styles.gameContainer : ""} ${styles.comboGrid}`}>
-        {ranks.map(rank => (
+      <div
+        className={`${gameMode ? styles.gameContainer : ""} ${styles.comboGrid
+          }`}
+      >
+        {ranks.map((rank) => (
           <React.Fragment key={rank}>
-            <div className={`${styles.answer} ${styles.percentageCombo}`} style={{ textAlign: "right", }}>
-              ({formatNumber(comboInfo1.combinations !== 0 ? Math.round((comboInfo1.combinationsByRank[rank] / comboInfo1.combinations) * 1000) / 10 : 0)}%)
+            <div
+              className={`${styles.answer} ${styles.percentageCombo}`}
+              style={{ textAlign: "right" }}
+            >
+              (
+              {formatNumber(
+                comboInfo1.combinations !== 0
+                  ? Math.round(
+                    (comboInfo1.combinationsByRank[rank] /
+                      comboInfo1.combinations) *
+                    1000,
+                  ) / 10
+                  : 0,
+              )}
+              %)
             </div>
             <div style={{ textAlign: "center" }} className={`${styles.answer}`}>
               {comboInfo1.combinationsByRank[rank]}
             </div>
-            <div><div className={`${styles.g1} ${styles.rank}`}>{rank}</div></div>
+            <div>
+              <div className={`${styles.g1} ${styles.rank}`}>{rank}</div>
+            </div>
             <div style={{ textAlign: "center" }}>
               <div style={{ opacity: 0.5 }}>W/ 1</div>
             </div>
-            <div><div className={`${styles.g2} ${styles.rank}`}>{rank}</div></div>
+            <div>
+              <div className={`${styles.g2} ${styles.rank}`}>{rank}</div>
+            </div>
             <div style={{ textAlign: "center" }} className={`${styles.answer}`}>
               {comboInfo2.combinationsByRank[rank]}
             </div>
             <div className={`${styles.answer} ${styles.percentageCombo}`}>
-              ({formatNumber(comboInfo2.combinations !== 0 ? Math.round((comboInfo2.combinationsByRank[rank] / comboInfo2.combinations) * 1000) / 10 : 0)}%)
+              (
+              {formatNumber(
+                comboInfo2.combinations !== 0
+                  ? Math.round(
+                    (comboInfo2.combinationsByRank[rank] /
+                      comboInfo2.combinations) *
+                    1000,
+                  ) / 10
+                  : 0,
+              )}
+              %)
             </div>
           </React.Fragment>
         ))}
       </div>
-    </main >
+    </main>
   );
 }
